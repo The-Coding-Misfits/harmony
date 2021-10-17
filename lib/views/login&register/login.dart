aa009fa879717cb4100d7fb829734f5c1cc9eeec
Göktürk Büyüktuna <gokturkbuyuktuna00@gmail.com>
2021-10-17 20:40:04 +0300

    cleander and divided 👌
    kodu biraz daha bölüp future use a göre tasarlarsan süper olur bro

--- lib/views/login&register/login.dart
+++ lib/views/login&register/login.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:harmony/services/auth_service.dart';
 import 'package:harmony/utilites/constants.dart';
+import 'package:harmony/utilites/login_state.dart';
+import 'package:harmony/widgets/login_register/harmony_shiny_button.dart';
+import 'package:harmony/widgets/login_register/harmony_log_input_field.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,85 +11,94 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
+
+  final AuthService authService = new AuthService();
+
+  HarmonyLogInput emailInputField = HarmonyLogInput(
+      const Icon(Icons.person, size: 25, color: Colors.grey),
+      "Email Address",
+      false,
+
+  );
+  HarmonyLogInput passwordInputField = HarmonyLogInput(
+      const Icon(Icons.lock, size: 25, color: Colors.grey),
+      "Password",
+      true
+  );
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _inputField(const Icon(Icons.person, size: 25, color: Colors.grey),
-                "Email Address",
-                false
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ///Logo section
+          Expanded(
+            flex: 4,
+            child: Hero(
+              tag: "harmony_logo",
+              child: Image.asset("assets/images/harmony.png"),
             ),
-            _inputField(const Icon(Icons.lock, size: 25, color: Colors.grey),
-                "Password",
-                true
+          ),
+          ///Input section
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: [
+                emailInputField,
+                passwordInputField,
+              ],
             ),
-            Container(
-              height: 60,
-              width: double.infinity,
-              margin: const EdgeInsets.only(top: 15, bottom: 50, left: 20, right: 20),
-              decoration: const BoxDecoration(
-                  color: Color(0xff00CA9D),
-                  borderRadius: BorderRadius.all(Radius.circular(40)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xff00a781),
-                      blurRadius: 10,
-                      offset: Offset(0,5),
-                      spreadRadius: 0,
-                    ),
-                  ]
-              ),
-              child: TextButton(
-                onPressed: () => {
-                  Navigator.pushReplacementNamed(
-                    context,
-                    kMainPageRouteName,
-                  )
-                },
-                child: const Text(
-                  "Login",
+          ),
+          ///Button
+          Flexible(
+            flex: 2,
+            child: HarmonyShinyButton(
+              "Login",
+                () => tapOnLogButton(context),
+            )
+          ),
+          ///Register
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 20,),
+                Text(
+                  "Not a member yet?",
                   style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20
+                    fontSize: 14,
+                  ) ,
+                ),
+                SizedBox(height: 5,),
+                GestureDetector(
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xff00CA9D)
+                    ),
                   ),
                 ),
-              ),
+
+
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
+  }
+  void tapOnLogButton(BuildContext context) async{
+    LOGIN_STATE loginState = await authService.logUser(emailInputField.currText, passwordInputField.currText);
+    if(loginState == LOGIN_STATE.SUCCESSFUL){
+      Navigator.pushNamedAndRemoveUntil(context, kMainPageRouteName, (route) => false);
+    }
   }
 
-  Widget _inputField(Icon prefixIcon, String hintText, bool isPassword) {
-    return Container(
-      margin: const EdgeInsets.only(left: 10, right: 10),
-      child: TextField(
-        obscureText: isPassword,
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.symmetric(vertical: 25),
-          hintText: hintText,
-          hintStyle: const TextStyle(
-            color: Color(0xff5F5D70),
-          ),
-          fillColor: const Color(0xffFBFAFF),
-          filled: true,
-          prefixIcon: prefixIcon,
-          prefixIconConstraints: const BoxConstraints(
-            minWidth: 50,
-          ),
-          enabledBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.white),
-          ),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.white),
-          ),
-        ),
-      ),
-    );
-  }
 }
---
+++ lib/widgets/login_register/harmony_shiny_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HarmonyShinyButton extends StatelessWidget {
+  final String text;
+  final Function() onPress;
+  final double size;
+
+
+  HarmonyShinyButton(this.text, this.onPress, {this.size : 60});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: double.infinity,
+      margin: const EdgeInsets.only(top: 15, bottom: 50, left: 20, right: 20),
+      decoration: const BoxDecoration(
+          color: Color(0xff00CA9D),
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xff00a781),
+              blurRadius: 10,
+              offset: Offset(0,5),
+              spreadRadius: 0,
+            ),
+          ]
+      ),
+      child: TextButton(
+        onPressed: onPress,
+        child: Text(
+          text,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 20
+          ),
+        ),
+      ),
+    );
+  }
+}
