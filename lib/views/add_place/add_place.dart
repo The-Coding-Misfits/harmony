08d6b68fb530f53a31a913cbfbf7098b04e4c8c3
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-10-23 19:30:03 +0300

    added the gridview thing to add_place

--- lib/views/add_place/add_place.dart
+++ lib/views/add_place/add_place.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:harmony/utilites/gridviewitem.dart';
 
 class AddPlace extends StatefulWidget {
   const AddPlace({Key? key}) : super(key: key);
@@ -8,6 +10,23 @@ class AddPlace extends StatefulWidget {
 }
 
 class AddPlaceState extends State<AddPlace> {
+  int optionSelected = 0;
+
+  void checkOption(int index) {
+    setState(() {
+      optionSelected = index;
+    });
+  }
+
+  List items = [
+    const Icon(Icons.backpack),
+    const FaIcon(FontAwesomeIcons.bicycle),
+    const FaIcon(FontAwesomeIcons.swimmer),
+    const FaIcon(FontAwesomeIcons.running),
+    const FaIcon(FontAwesomeIcons.campground),
+    const FaIcon(FontAwesomeIcons.paw)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,13 +39,42 @@ class AddPlaceState extends State<AddPlace> {
             onPressed: () {
 
             },
-            child: const Text("Save"),
+            child: const Text("Save", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
             style: ButtonStyle(
               foregroundColor: MaterialStateProperty.all(Colors.white),
               backgroundColor: MaterialStateProperty.all(Colors.transparent)
             ),
           )
         ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(top: 5, left: 5),
+                child: Text("What can you do here?", style: TextStyle(fontSize: 18)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              child: GridView.count(
+                crossAxisCount: 6,
+                mainAxisSpacing: 6,
+                crossAxisSpacing: 6,
+                shrinkWrap: true,
+                children: [
+                  for (int i = 0; i < items.length; i++) GridViewItem(
+                      icon: items[i],
+                      selected: i + 1 == optionSelected,
+                      onTap: () => checkOption(i + 1)
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
