df87a7f7b82fcb3c869a0b3340c2a6619d7a0241
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-10-24 13:43:49 +0300

    added bottom bar to account page

--- lib/views/account/account_page.dart
+++ lib/views/account/account_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:harmony/utilites/page_enum.dart';
+import 'package:harmony/widgets/general_use/harmony_bottom_navigation_bar.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: HarmonyBottomNavigationBar(
+          PAGE_ENUM.ACCOUNT_PAGE
+      ),
+    );
   }
 }
