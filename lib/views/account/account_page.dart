a581fa4d8a9b7df4a53bd08e6cecc9f4c245e382
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-10-24 14:15:31 +0300

    added avatar and name to account_page

--- lib/views/account/account_page.dart
+++ lib/views/account/account_page.dart
@@ -8,6 +8,25 @@ class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  child: Image.asset("assets/images/dummy-profile-pic.png", scale: 3),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Text("Emir Sürmen", style: TextStyle(fontSize: 20)),
+              )
+            ],
+          ),
+        )
+      ),
       bottomNavigationBar: HarmonyBottomNavigationBar(
           PAGE_ENUM.ACCOUNT_PAGE
       ),
