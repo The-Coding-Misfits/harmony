cb6d46c219767c7e77bddb0dab2bf36bb523a90d
emirsurmen <emirsurmen@gmail.com>
2021-11-06 17:19:31 +0300

    changed map tilelayer

--- lib/views/add_place/add_place.dart
+++ lib/views/add_place/add_place.dart
@@ -85,8 +85,11 @@ class AddPlaceState extends State<AddPlace> {
           ),
           layers: [
             TileLayerOptions(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c'],
+              urlTemplate: "https://api.mapbox.com/styles/v1/emirsurmen/ckvnw0v1m4hua14pigfs0c0k0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnc0bWUwODhjMzFrZ2g4c3FhdXh6In0.hj6d-tS9kiDepCIjqJOG5A",
+              additionalOptions: {
+                "accessToken": "pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnk1MTcxYmtlMzJrbDdncWp2YXZvIn0.cU2tnWQoUKHWZGYevlnb6w",
+                "id": "mapbox.satellite"
+              }
             ),
             MarkerLayerOptions(
               markers: [
