740466bfe32453722d36a1d704ec47856d18a6d6
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-11-09 20:59:47 +0300

    added map to spot info and changed the map marker

--- lib/views/place/spot_info.dart
+++ lib/views/place/spot_info.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:harmony/models/place.dart';
+import 'package:latlong2/latlong.dart';
 
 class SpotInfo extends StatefulWidget {
 
@@ -19,18 +22,51 @@ class SpotInfoState extends State<SpotInfo> {
     String imageUrl = args["imageUrl"];
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 0,
         centerTitle: true,
         title: const Text("Spot Details", style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.black12,
         foregroundColor: Colors.black,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 10),
-        child: Column(
-          children: [
-            Align(
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            child: SizedBox(
+              height: 250,
+              width: MediaQuery.of(context).size.width,
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(place.point.latitude, place.point.longitude),
+                  zoom: 15.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                      urlTemplate: "https://api.mapbox.com/styles/v1/emirsurmen/ckvnyv8r5dhwz14nnxz6oqfbk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnc0bWUwODhjMzFrZ2g4c3FhdXh6In0.hj6d-tS9kiDepCIjqJOG5A",
+                      additionalOptions: {
+                        "accessToken": "pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnk1MTcxYmtlMzJrbDdncWp2YXZvIn0.cU2tnWQoUKHWZGYevlnb6w",
+                        "id": "mapbox.satellite"
+                      }
+                  ),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                          width: 80.0,
+                          height: 80.0,
+                          point: LatLng(place.point.latitude, place.point.longitude),
+                          builder: (ctx) => const Icon(FontAwesomeIcons.dotCircle, color: Colors.red, size: 30,)
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10, left: 10),
+            child: Align(
               alignment: Alignment.topLeft,
               child: Text(
                 place.name,
@@ -39,20 +75,16 @@ class SpotInfoState extends State<SpotInfo> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(top: 10, left: 10),
               child: Align(
-                alignment: Alignment.topLeft,
-                child: SizedBox(
-                  width: 500,
-                  height: 300 ,
-                  child: Image.asset("assets/images/dummy-national-park.jpg"),
-                ),
+                  alignment: Alignment.topLeft,
+                  child: Image.asset("assets/images/dummy-national-park.jpg", scale: 6)
               )
-            )
-          ],
-        ),
-      )
+          )
+        ],
+      ),
     );
   }
 }
