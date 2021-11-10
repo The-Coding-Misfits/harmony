9a701bfe79b59838f587de59eb03e8362604bf57
emirsurmen <emirsurmen@gmail.com>
2021-11-10 23:11:36 +0300

    spot info scroll brr

--- lib/views/place/spot_info.dart
+++ lib/views/place/spot_info.dart
@@ -34,92 +34,94 @@ class SpotInfoState extends State<SpotInfo> {
         backgroundColor: const Color(0x11000000),
         foregroundColor: Colors.black,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            child: SizedBox(
-              height: 250,
-              width: MediaQuery.of(context).size.width,
-              child: FlutterMap(
-                options: MapOptions(
-                  center: LatLng(place.point.latitude, place.point.longitude),
-                  zoom: 15.0,
-                ),
-                layers: [
-                  TileLayerOptions(
-                      urlTemplate: "https://api.mapbox.com/styles/v1/emirsurmen/ckvnyv8r5dhwz14nnxz6oqfbk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnc0bWUwODhjMzFrZ2g4c3FhdXh6In0.hj6d-tS9kiDepCIjqJOG5A",
-                      additionalOptions: {
-                        "accessToken": "pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnk1MTcxYmtlMzJrbDdncWp2YXZvIn0.cU2tnWQoUKHWZGYevlnb6w",
-                        "id": "mapbox.satellite"
-                      }
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+              child: SizedBox(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                child: FlutterMap(
+                  options: MapOptions(
+                    center: LatLng(place.point.latitude, place.point.longitude),
+                    zoom: 15.0,
                   ),
-                  MarkerLayerOptions(
-                    markers: [
-                      Marker(
-                          width: 80.0,
-                          height: 80.0,
-                          point: LatLng(place.point.latitude, place.point.longitude),
-                          builder: (ctx) => const Icon(FontAwesomeIcons.dotCircle, color: Colors.red, size: 30,)
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 10),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                place.name,
-                style: const TextStyle(
-                    fontSize: 20
-                ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-                padding: const EdgeInsets.only(bottom: 5, left: 10, top: 5),
-                child: Row(
-                  children: [
-                    RatingWidget(place.rating),
-                    Text(" ${place.rating}/5.0 • ${distance}KM Nearby",
-                        style: const TextStyle(
-                            fontSize: 16,
-                            color: Color(0xff6a6a6a),
-                            fontWeight: FontWeight.bold
-                        )
+                  layers: [
+                    TileLayerOptions(
+                        urlTemplate: "https://api.mapbox.com/styles/v1/emirsurmen/ckvnyv8r5dhwz14nnxz6oqfbk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnc0bWUwODhjMzFrZ2g4c3FhdXh6In0.hj6d-tS9kiDepCIjqJOG5A",
+                        additionalOptions: {
+                          "accessToken": "pk.eyJ1IjoiZW1pcnN1cm1lbiIsImEiOiJja3Zudnk1MTcxYmtlMzJrbDdncWp2YXZvIn0.cU2tnWQoUKHWZGYevlnb6w",
+                          "id": "mapbox.satellite"
+                        }
+                    ),
+                    MarkerLayerOptions(
+                      markers: [
+                        Marker(
+                            width: 80.0,
+                            height: 80.0,
+                            point: LatLng(place.point.latitude, place.point.longitude),
+                            builder: (ctx) => const Icon(FontAwesomeIcons.dotCircle, color: Colors.red, size: 30,)
+                        ),
+                      ],
                     ),
                   ],
-                )
+                ),
+              ),
             ),
-          ),
-          Padding(
+            Padding(
               padding: const EdgeInsets.only(top: 10, left: 10),
               child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Image.network(imageUrl, scale: 6)
-              )
-          ),
-          Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Align(
-                  alignment: Alignment.center,
-                  child: HarmonyShinyButton(
-                    "Get directions",
-                    () {
-                      MapsLauncher.launchCoordinates(place.point.latitude, place.point.longitude);
-                    },
-                    size: 50,
+                alignment: Alignment.topLeft,
+                child: Text(
+                  place.name,
+                  style: const TextStyle(
+                      fontSize: 20
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                  padding: const EdgeInsets.only(bottom: 5, left: 10, top: 5),
+                  child: Row(
+                    children: [
+                      RatingWidget(place.rating),
+                      Text(" ${place.rating}/5.0 • ${distance}KM Nearby",
+                          style: const TextStyle(
+                              fontSize: 16,
+                              color: Color(0xff6a6a6a),
+                              fontWeight: FontWeight.bold
+                          )
+                      ),
+                    ],
                   )
-              )
-          )
-        ],
-      ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 10, left: 10),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Image.network(imageUrl, scale: 6)
+                )
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Align(
+                    alignment: Alignment.center,
+                    child: HarmonyShinyButton(
+                      "Get directions",
+                          () {
+                        MapsLauncher.launchCoordinates(place.point.latitude, place.point.longitude);
+                      },
+                      size: 50,
+                    )
+                )
+            )
+          ],
+        ),
+      )
     );
   }
 }
