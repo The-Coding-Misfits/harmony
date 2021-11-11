19f82e21da636f56ecdbe2e050005e7eacb3d749
Göktürk <87906890+gokturkDev@users.noreply.github.com>
2021-11-12 00:10:25 +0300

    nearby

--- lib/views/place/near_place.dart
+++ lib/views/place/near_place.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:harmony/widgets/filter/filter_sheet/filter_model.dart';
 import 'package:harmony/widgets/filter/filter_sheet/filter_top_bar.dart';
 import 'package:harmony/widgets/filter/filter_sheet/mixin&interface/filter_sheet_creator.dart';
+import 'package:harmony/widgets/filter/filter_sheet/mixin&interface/uses_filter_sheet.dart';
 
 class NearbyPage extends StatefulWidget {
   const NearbyPage({Key? key}) : super(key: key);
@@ -10,8 +11,9 @@ class NearbyPage extends StatefulWidget {
   _NearbyPageState createState() => _NearbyPageState();
 }
 
-class _NearbyPageState extends State<NearbyPage> with FilterSheetCreator{
+class _NearbyPageState extends State<NearbyPage> with FilterSheetCreator implements UsesFilterSheet{
 
+  @override
   FilterModel filterModel = FilterModel();
 
   @override
@@ -21,11 +23,28 @@ class _NearbyPageState extends State<NearbyPage> with FilterSheetCreator{
         child: Column(
           children: [
             FilterTopBar(
-
+              launchFilterSheet,
+              "Nearby"
             ),
           ],
         ),
       )
     );
+  }
+
+  @override
+  void launchFilterSheet() {
+    toFilterSheet(
+        context,
+        filterModel,
+        onUpdatedFilters
+    );
+  }
+
+  @override
+  void onUpdatedFilters(FilterModel filterModel) {
+    setState(() {
+      this.filterModel = filterModel;
+    });
   }
 }
