0084276fc9cfa013a02f011a27715d9b7eda31b5
emirsurmen <emirsurmen@gmail.com>
2021-11-12 20:49:02 +0300

    changed favorite spots background color

--- lib/widgets/account_page/user_favorites.dart
+++ lib/widgets/account_page/user_favorites.dart
@@ -17,44 +17,61 @@ class UserFavoritesState extends State<UserFavorites> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2
-      ),
-      shrinkWrap: true,
-      itemCount: user!.favoritesID.length,
-      itemBuilder: (BuildContext context, int index) {
-        var placeId = user!.favoritesID[index];
-
-        return FutureBuilder(
-          future: FireStoreService().getPlaceFromId(placeId),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              Place place = snapshot.data;
+    if (user!.favoritesID.isEmpty) {
+      return Expanded(
+        child: Container(
+          color: const Color(0xffefefef),
+          child: const Align(
+            alignment: Alignment.center,
+            child: Text("No favorites!"),
+          ),
+        )
+      );
+    } else {
+      return Expanded(
+        child: Container(
+          color: const Color(0xffefefef),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2
+            ),
+            shrinkWrap: true,
+            itemCount: user!.favoritesID.length,
+            itemBuilder: (BuildContext context, int index) {
+              var placeId = user!.favoritesID[index];
 
               return FutureBuilder(
-                future: FireStoreService().getCoverFromId(placeId),
+                future: FireStoreService().getPlaceFromId(placeId),
                 builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
                   if (snapshot.connectionState == ConnectionState.done) {
-                    return PlaceCard(
-                      contentSize: 17,
-                      imageUrl: snapshot.data,
-                      place: place,
+                    Place place = snapshot.data;
+
+                    return FutureBuilder(
+                      future: FireStoreService().getCoverFromId(placeId),
+                      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return PlaceCard(
+                            contentSize: 17,
+                            imageUrl: snapshot.data,
+                            place: place,
+                          );
+                        }
+
+                        return const Align(
+                          alignment: Alignment.center,
+                          child: Text("Loading spot card..."),
+                        );
+                      },
                     );
                   }
 
-                  return const Align(
-                    alignment: Alignment.center,
-                    child: Text("Loading spot card..."),
-                  );
+                  return const Text("Loading spot...");
                 },
               );
-            }
-
-            return const Text("Loading spot...");
-          },
-        );
-      },
-    );
+            },
+          ),
+        )
+      );
+    }
   }
 }
