7a7f1dc03d583f985c679fcdf7f67b2deb16f95b
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-11-15 17:02:21 +0300

    padding between image and rating (spot info) now equal

--- lib/views/place/spot_info.dart
+++ lib/views/place/spot_info.dart
@@ -136,7 +136,7 @@ class SpotInfoState extends State<SpotInfo> {
               ),
             ),
             Padding(
-                padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+                padding: const EdgeInsets.only(top: 5, left: 10, right: 10),
                 child: Align(
                     alignment: Alignment.topLeft,
                     child: imageUrlWidget
