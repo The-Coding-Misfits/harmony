8e5f85600d2c5850d8ebc0d42ce27536238023d2
emirsurmen <emirsurmen@gmail.com>
2021-11-15 14:57:50 +0300

    changed review card and added getuserfromid func

--- lib/widgets/general_use/review_card.dart
+++ lib/widgets/general_use/review_card.dart
@@ -1,55 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:harmony/models/review.dart';
+import 'package:harmony/models/user.dart';
 import 'package:harmony/services/firestore.dart';
+import 'package:harmony/widgets/general_use/rating_widget.dart';
 
 class ReviewCard extends StatelessWidget {
   final Review review;
 
   ReviewCard(this.review, {Key? key}) : super(key: key);
 
-  String? pfpUrl;
-
-  getPfp(String userId) async {
-    if (pfpUrl == null) {
-      pfpUrl = await FireStoreService().getPfpFromId(userId);
-      return pfpUrl;
-    } else {
-      return pfpUrl;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    Widget pfpWidget = FutureBuilder(
-      future: getPfp(review.authorID),
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        if (snapshot.hasError) {
-          return Image.asset("assets/images/dummy-profile-pic.png");
-        } else if (snapshot.connectionState == ConnectionState.done) {
-          return Image.network(snapshot.data, fit: BoxFit.fill);
-        } else {
-          return Container(color: Colors.grey);
-        }
-      },
-    );
-
     return Padding(
       padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
-      child: Column(
-        children: [
-          ListTile(
-            leading: SizedBox(
-              width: 50,
-              height: 50,
-              child: CircleAvatar(
-                radius: 100,
-                child: pfpWidget,
-              ),
-            ),
-            title: Text(),
+      child: Card(
+        color: Colors.white,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: 100
           ),
-        ]
-      ),
+          child: Column(
+              children: [
+                ListTile(
+                  title: FutureBuilder(
+                    future: FireStoreService().getUserFromId(review.authorID),
+                    builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        HarmonyUser user = snapshot.data;
+
+                        return Text(user.username);
+                      } else {
+                        return const Text("Loading...");
+                      }
+                    },
+                  ),
+                  subtitle: RatingWidget(review.rating.toDouble(), 10),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5, left: 5, right: 5),
+                  child: Text(review.content),
+                )
+              ]
+          ),
+        )
+      )
     );
   }
 }
