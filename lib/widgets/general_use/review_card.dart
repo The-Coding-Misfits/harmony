e63e8dfeda5d69c782e0f87ed5999650dd59e5e3
Göktürk Büyüktuna <gokturkbuyuktuna00@gmail.com>
2021-11-16 19:03:56 +0300

    fix: review card in spot info pushes to author profile

--- lib/widgets/general_use/review_card.dart
+++ lib/widgets/general_use/review_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:harmony/models/place.dart';
 import 'package:harmony/models/review.dart';
+import 'package:harmony/models/user.dart';
 import 'package:harmony/services/firestore.dart';
 import 'package:harmony/utilites/constants.dart';
+import 'package:harmony/views/account/account_page.dart';
 import 'package:harmony/widgets/general_use/rating_widget.dart';
 
 class ReviewCard extends StatelessWidget {
@@ -14,7 +16,7 @@ class ReviewCard extends StatelessWidget {
       forSpotInfo = false,
       super(key: key);
 
-  ReviewCard.forSpotInfo(this.review, this.contentSize,) :
+  const ReviewCard.forSpotInfo(this.review, this.contentSize,) :
       forSpotInfo = true;
 
   @override
@@ -22,18 +24,8 @@ class ReviewCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 5, right: 5, top: 5),
       child: GestureDetector(
-        onTap: () async {
-          Place place = await FireStoreService().getPlaceFromId(review.placeID);
-
-          Navigator.pushNamed(
-              context,
-              kSpotInfoRouteName,
-              arguments: {
-                "place": place,
-                "distance": null,
-                "imageUrl": null,
-              }
-          );
+        onTap: () {
+          handleTap(context);
         },
         child: Card(
           color: Colors.white,
@@ -76,5 +68,28 @@ class ReviewCard extends StatelessWidget {
         ),
       )
     );
+  }
+  void handleTap(BuildContext context) async{
+    Place place = await FireStoreService().getPlaceFromId(review.placeID);
+    if(forSpotInfo){
+      HarmonyUser user = await FireStoreService().getUserFromId(review.authorID);
+      //not best practice but whatever
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AccountPage(
+              user,
+          ))
+      );
+    }else{
+      Navigator.pushNamed(
+          context,
+          kSpotInfoRouteName,
+          arguments: {
+            "place": place,
+            "distance": null,
+            "imageUrl": null,
+          }
+      );
+    }
   }
 }
