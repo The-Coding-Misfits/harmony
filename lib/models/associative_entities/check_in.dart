3c6518c3890ae9767590853156d1197fc7c25045
Göktürk Büyüktuna <gokturkbuyuktuna00@gmail.com>
2021-11-16 12:24:34 +0300

    refactor: toString

--- lib/models/associative_entities/check_in.dart
+++ lib/models/associative_entities/check_in.dart
@@ -3,4 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class CheckIn{
   Timestamp time;
   CheckIn(this.time);
+
+  @override
+  String toString(){
+    return time.toString();
+  }
 }
