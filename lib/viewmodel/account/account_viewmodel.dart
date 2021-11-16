a4b105b9d434c61f11f73554fde9e76734996b60
Göktürk Büyüktuna <gokturkbuyuktuna00@gmail.com>
2021-11-16 17:13:33 +0300

    chore: sort check ins to chunks

--- lib/viewmodel/account/account_viewmodel.dart
+++ lib/viewmodel/account/account_viewmodel.dart
@@ -5,47 +5,43 @@ import 'package:harmony/viewmodel/account/check_in_chunk.dart';
 
 class AccountPageViewModel {
 
-  List<CheckInChunk> getCheckInsByWeek(HarmonyUser user){
+  List<CheckInChunk> getChunks(HarmonyUser user){
     List<CheckIn> checkIns = _getCheckIns(user);
     if(checkIns.isEmpty) return [];
-
+    List<Timestamp> chunkStartTimestamps = divideTheCheckInTimeFrame(checkIns);
+    return sortCheckInsToChunks(chunkStartTimestamps, checkIns);
   }
 
   List<CheckIn> _getCheckIns(HarmonyUser user){
     List<CheckIn> checkIns = user.checkIns;
-    _sortCheckInsByTime(checkIns);
+    checkIns = _sortCheckInsByTime(checkIns);
     return checkIns;
   }
-  void _sortCheckInsByTime(List<CheckIn> checkIns){
+  List<CheckIn> _sortCheckInsByTime(List<CheckIn> checkIns){
     checkIns.sort(
         _compareTwoCheckIns
     );
+    //checkIns = checkIns.reversed as List<CheckIn>; // since we compare if bigger
+    print(checkIns);
+    return checkIns;
   }
 
   int _compareTwoCheckIns(CheckIn firstCheckIn, CheckIn secondCheckIn){
     return firstCheckIn.compareTo(secondCheckIn);
   }
 
-
-  List<CheckInChunk> getChunks(List<CheckIn> checkIns){
-
-
-
-
-  }
-
   List<Timestamp> divideTheCheckInTimeFrame(List<CheckIn> checkIns){
     const NUM_DIVISIONS_TIMEFRAME = 15;
     Timestamp firstCheckInTime = checkIns.first.time;
     Timestamp lastCheckInTime = checkIns.last.time;
-    return pointChunks(NUM_DIVISIONS_TIMEFRAME, firstCheckInTime, lastCheckInTime);
+    return getTheChunkStartTimeStamps(NUM_DIVISIONS_TIMEFRAME, firstCheckInTime, lastCheckInTime);
   }
 
-  List<Timestamp> pointChunks(int NUM_DIVISIONS_TIMEFRAME,Timestamp firstCheckInTime, Timestamp lastCheckInTime ){
+  List<Timestamp> getTheChunkStartTimeStamps(int NUM_DIVISIONS_TIMEFRAME,Timestamp firstCheckInTime, Timestamp lastCheckInTime ){
     int totalSecondsBetweenCheckins = lastCheckInTime.seconds - firstCheckInTime.seconds;
     int oneChunkTime = totalSecondsBetweenCheckins ~/ NUM_DIVISIONS_TIMEFRAME;
     List<Timestamp> timeChunks = [];
-    for(int i = 1; i <= NUM_DIVISIONS_TIMEFRAME; i++){
+    for(int i = 0; i < NUM_DIVISIONS_TIMEFRAME; i++){
       int timeOfChunk = firstCheckInTime.seconds + (oneChunkTime * i);
       timeChunks.add(
         Timestamp(timeOfChunk, 0)
@@ -53,6 +49,37 @@ class AccountPageViewModel {
     }
     return timeChunks;
 
+  }
+
+  List<CheckInChunk> sortCheckInsToChunks(List<Timestamp> chunkStartTimestamps, List<CheckIn> checkIns){
+    List<CheckInChunk> checkInChunks = [];
+    int numberOfPointsInChunk = 0;
+    int currIndexOfTimestamps = 1;// start from second time stamp since we are checking smaller
+    for(int i = 0; i < checkIns.length; i++){
+      Timestamp nextChunkStartTimeStamp = chunkStartTimestamps[currIndexOfTimestamps];
+      CheckIn checkIn = checkIns[i];
+      int comparisonResult = checkIn.time.compareTo(nextChunkStartTimeStamp);
+      if (comparisonResult == -1){//meaning checkIn time is smaller than next time stamp and belongs to this chunk
+        numberOfPointsInChunk++;
+      }
+      else if(comparisonResult == 1  || comparisonResult == 0){ //meaning checkIn time is bigger or equal to the next timestamp therefore belongs to next chunk
+        try{
+          nextChunkStartTimeStamp = chunkStartTimestamps[currIndexOfTimestamps++];
+          CheckInChunk chunkResolved = CheckInChunk(numberOfPointsInChunk);
+          checkInChunks.add(chunkResolved);
+          numberOfPointsInChunk = 0;
+          i--; //since we want to loop again with this check in
+        }catch(_){
+          //catches for out of bounds exception in line 67
+          //if there are no more time chunks meaning this is the last chunk
+          numberOfPointsInChunk = checkIns.getRange(i, checkIns.length).length;
+          CheckInChunk chunkResolved = CheckInChunk(numberOfPointsInChunk);
+          checkInChunks.add(chunkResolved);
+          break;
+        }
+      }
+    }
+    return checkInChunks;
   }
 
 
