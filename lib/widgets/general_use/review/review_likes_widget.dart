1c411e4724d27081f1e841d69b9373ed33d2c4ab
Göktürk Büyüktuna <gokturkbuyuktuna00@gmail.com>
2021-11-20 21:36:36 +0300

    chore: review likes widget now competes for pointer events

--- lib/widgets/general_use/review/review_likes_widget.dart
+++ lib/widgets/general_use/review/review_likes_widget.dart
@@ -4,38 +4,49 @@ import 'package:harmony/models/review.dart';
 import 'package:harmony/models/user.dart';
 import 'package:harmony/services/auth_service.dart';
 
-class ReviewLikesWidget extends StatelessWidget {
+class ReviewLikesWidget extends StatefulWidget {
   final Review review;
 
   const ReviewLikesWidget(this.review);
 
   @override
+  State<ReviewLikesWidget> createState() => _ReviewLikesWidgetState();
+}
+
+class _ReviewLikesWidgetState extends State<ReviewLikesWidget> {
+  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Icon(
-          didCurrentUserLikedThisReview() ? Icons.favorite : Icons.favorite_border_outlined,
-          color: Colors.red,
+        GestureDetector(
+          onTap: handleTap,
+          child: Icon(
+            didCurrentUserLikedThisReview() ? Icons.favorite : Icons.favorite_border_outlined,
+            color: Colors.red,
+          ),
         ),
         Text(
-          review.getLikeCount().toString(),
+          widget.review.getLikeCount().toString(),
           style: const TextStyle(
-            fontSize: 7
+            fontSize: 12
           ),
         )
       ],
     );
   }
 
-
   bool didCurrentUserLikedThisReview(){
     HarmonyUser currentUser = AuthService.currHarmonyUser!;
-    for(Like like in review.likes){
+    for(Like like in widget.review.likes){
       if (like.userId == currentUser.id){
         return true;
       }
     }
     return false;
+  }
+
+  void handleTap(){
+
   }
 }
